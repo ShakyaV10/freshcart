4ee71e5f02158b39d488ee7c727a9cf59bc2c415
ShakyaV10 <b420060@iiit-bh.ac.in>
2023-06-22 12:16:15 +0530

    Header3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div> 
-   <Header/>
+      <Header/>
+      <main className='pt-16'>
+        <Outlet />
+      </main>
     </div>
   );
 }
