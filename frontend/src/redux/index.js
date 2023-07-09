f070dd637d67b551d504c6b5ad17ff9b39343044
Chinmaya <pradhanchinmaya108@gmail.com>
2023-07-10 02:16:13 +0530

    CP/10-07

--- frontend/src/redux/index.js
+++ frontend/src/redux/index.js
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userSliceReducer from "./userSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import userSliceReducer from "./userSlice";
 
 export const store = configureStore({
     reducer: {
