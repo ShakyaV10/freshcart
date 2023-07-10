46b2e5cf911428ad4a8fbd18a97c0d704d08b17b
Chinmaya <pradhanchinmaya108@gmail.com>
2023-07-10 21:44:51 +0530

    CP/10-07/4

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
           <p className="text-sm font-medium text-slate-900">Bike Delivery</p>
           <img src="https://cdn-icons-png.flaticon.com/512/2972/2972185.png" className="h-7"/>
         </div>
-        <h2 className="text-4xl md:text-7xl font-bold py-3">The Fastest Delivery to <span className="text-green-600 text-">Your Kitchen</span></h2>
+        <h2 className="text-4xl md:text-7xl font-bold py-3">Fastest Delivery by <span className="text-green-600 text-">Fresh Cart</span></h2>
         <p className="py-3 text-base">Fresh Cart is an online e-commerce site specializing in grocery and food items, offering a convenient and hassle-free shopping experience. With a wide selection of fresh produce, pantry staples, and gourmet treats, customers can easily browse and order their favorite products from the comfort of their homes. The platform ensures the highest quality and freshness of its products, partnering with trusted local suppliers and adhering to strict quality control measures. </p>
         <button className="font-bold bg-green-500 text-slate-100 px-4 py-2 rounded">Order Now</button>
       </div>
